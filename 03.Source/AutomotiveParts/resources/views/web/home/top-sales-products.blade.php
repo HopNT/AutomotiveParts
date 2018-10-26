536615d95366b8616d1d0eb10f125b906d3f60fc
TuPeo <nguyenthanhtu94@gmail.com>
2018-10-27 01:41:44 +0700

    update code

--- 03.Source/AutomotiveParts/resources/views/web/home/top-sales-products.blade.php
+++ 03.Source/AutomotiveParts/resources/views/web/home/top-sales-products.blade.php
@@ -1,3 +1,4 @@
+
 <div id="make-icons" class="shop-your-make mb-20px">
 <!--    <h3 class="title">Genuine Parts Online Catalogs</h3>-->
     <div class="row">
@@ -5,9 +6,16 @@
             <div class="col-md-3 col-sm-4 col-xs-6 mb-20px">
                 <div class="item item-effect">
                     <!-- Item image -->
-                    <a title="Online Catalog BMW OEM Parts" href="{{route('view-accessory-detail', ['accessary_id' => $accessaryPrioritize->accessary_id])}}">
+                    <a title="{{$accessaryPrioritize->name_vi}}" href="{{route('view-accessory-detail', ['accessary_id' => $accessaryPrioritize->accessary_id])}}">
                         <div class="shop-logo"><img src="{{asset($accessaryPrioritize->photo_top)}}" alt="BMW online catalog" class="img-responsive"></div>
-                        <div class="shop-title">{{$accessaryPrioritize->name_vi}}</div>
+                        <div class="shop-title-2">
+                            <div class="sp-left">{{$accessaryPrioritize->code}}</div>
+                            <div class="sp-right">{{ $accessaryPrioritize->price ? number_format($accessaryPrioritize->price).'vnd' : 'N/A'}}</div>
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="shop-title">
+                                {{$accessaryPrioritize->name_vi}}
+                        </div>
                     </a>
                 </div>
             </div>
