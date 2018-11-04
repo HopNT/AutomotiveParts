be5775aa85204747b92c83c8371809e330d8a341
TuPeo <nguyenthanhtu94@gmail.com>
2018-11-05 02:00:11 +0700

    update source

--- 03.Source/AutomotiveParts/resources/views/web/home/catalog.blade.php
+++ 03.Source/AutomotiveParts/resources/views/web/home/catalog.blade.php
@@ -1,13 +1,18 @@
+<?php
+    $index = 0;
+    $index_child = 0;
+?>
 <div id="catalog" class="top-tabs mb-20px">
     <!-- <h3 class="title mb-20px">About Partsouq</h3> -->
     <ul class="nav nav-tabs row">
         @foreach($listCatalogPartsParent as $key => $catalogPartsParent)
-            <li class="col-sm-4"><a href="#tab{{$key + 1}}" data-toggle="tab" class="@if($key == 0)active show @endif ">{{$catalogPartsParent->name}}</a></li>
+            <li class="col-sm-4"><a href="#tab{{$key + 1}}" data-toggle="tab" class="tab-class @if($index == 0)active show @endif ">{{$catalogPartsParent->name}}</a></li>
+            <?php $index++; ?>
         @endforeach
     </ul>
     <div class="tab-content">
         @foreach($listCatalogPartsParent as $key => $catalogPartsParent)
-            <div class="tab-pane fade @if($key == 0) active show @endif" id="tab{{$key + 1}}">
+            <div class="tab-pane tab-class-child fade @if($index_child == 0) active show @endif" id="tab{{$key + 1}}">
                 <div class="row">
                     @foreach($catalogPartsParent->child as $key => $c)
                         <div class="col-sm-6 col-md-3" id="group-item">
@@ -16,6 +21,7 @@
                     @endforeach
                 </div>
             </div>
+            <?php $index_child++; ?>
         @endforeach
     </div>
 </div>
